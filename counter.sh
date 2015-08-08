01d0d34d656d06c5bd05c2e7c83f5fa649f002c8
Mykola Gurov <ngurov@gmail.com>
2015-08-08 21:42:15 +0200

    exit the loop

--- counter.sh
+++ counter.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -eu
 
+stop=${1:--1}
 cnt=1
 while [ $cnt -gt 0 ]; do
     echo "Count: $cnt."
-    let cnt=$cnt+1
+    if [ $cnt -eq $stop ]; then
+        cnt=0
+        echo "exiting"
+    else
+        let cnt=$cnt+1
+    fi
+
     sleep 1;
 done
